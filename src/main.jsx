998096305fa51088fb340e5eeb0c3fb894ab1b18
Alvarogranillo61 <145382442+Alvarogranillo61@users.noreply.github.com>
2026-03-26 23:29:44 -0600

    arreglar diseño

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
-import './modules/parking/styles/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@flaticon/flaticon-uicons/css/all/all.css';
+import './index.css';
+import './modules/parking/styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
